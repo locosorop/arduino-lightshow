95946f4fa16cec2787faac647a9269fdb8fddf99
Imani Dap <imani.dap@gmail.com>
2019-04-07 20:23:59 +0200

    fix(switch): switch a all times
    Fixed the switch on strobe problem by implementing the switch with an interrupt

--- main.cpp
+++ main.cpp
@@ -6,33 +6,28 @@ int switchBtnPin = 3;
 DigitalLed digiLed(13, 2); 
 AnalogLed anaLed(5, A0);
 
-int switchBtnState = 0;
-int prevSwitchBtnState = 0;
-
-void setup() {
-  Serial.begin(9600);
-  pinMode(switchBtnPin, INPUT); 
-}
-
-void loop() {
-  //TODO: interrupts instead of polling!
-  switchBtnState = digitalRead(switchBtnPin);
-  if(prevSwitchBtnState == 1 && switchBtnState == 0) {
-    if(digiLed.getCurrentStrategyType() == DigitalLed::DigitalLightType::Doorbell) {
+void switchStrategy() {
+  if(digiLed.getCurrentStrategyType() == DigitalLed::DigitalLightType::Doorbell) {
       digiLed.setStrategy(DigitalLed::DigitalLightType::Toggle); 
     } else {
       digiLed.setStrategy(DigitalLed::DigitalLightType::Doorbell); 
     } 
 
-    if(anaLed.getCurrentStrategyType() == AnalogLed::AnalogLightType::Dimmer) {
+  if(anaLed.getCurrentStrategyType() == AnalogLed::AnalogLightType::Dimmer) {
       anaLed.setStrategy(AnalogLed::AnalogLightType::Strobe);
     } else {
       anaLed.setStrategy(AnalogLed::AnalogLightType::Dimmer);
     }
-  }
+}
 
+void setup() {
+  Serial.begin(9600);
+  pinMode(switchBtnPin, INPUT); 
+  attachInterrupt(digitalPinToInterrupt(switchBtnPin), switchStrategy, RISING);
+}
+
+void loop() {
   anaLed.render();
   digiLed.render();
-  prevSwitchBtnState = switchBtnState;
 }
 
